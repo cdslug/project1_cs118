b2f64a507d6e816f41ba21e6ca18feaa7f59827d
Spencer Brett <spencer.a.brett@gmail.com>
2013-10-29 01:46:23 -0700

    Added support for multiple simultaneous connections. Still can't get the server working, so I'm not sure if it's working correctly.

--- src/server.c
+++ src/server.c
@@ -1,6 +1,14 @@
+/*
+ * server.c
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: spencer
+ */
+
+
 /* A simple server in the internet domain using TCP
-   The port number is passed as an argument 
-   This version runs forever, forking off a separate 
+   The port number is passed as an argument
+   This version runs forever, forking off a separate
    process for each connection
 */
 #include <stdio.h>
@@ -14,8 +22,8 @@
 #include <string.h>
 #include <assert.h>
 #include <time.h>
-#include "parse.h"
-#include "response.h"
+#include "include/parse.h"
+#include "include/response.h"
 
 #define MAX_FILE_SIZE 1024
 
@@ -30,7 +38,7 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, pid;
+     int sockfd, portno; //sockfd:portno -> server socket
      socklen_t clilen;
      struct sockaddr_in serv_addr, cli_addr;
 
@@ -39,7 +47,7 @@ int main(int argc, char *argv[])
          exit(1);
      }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);	//create socket
-     if (sockfd < 0) 
+     if (sockfd < 0)
         error("ERROR opening socket");
      memset((char *) &serv_addr, 0, sizeof(serv_addr));	//reset memory
      //fill in address info
@@ -47,96 +55,58 @@ int main(int argc, char *argv[])
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
-     
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+
+     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
               error("ERROR on binding");
-     
-     listen(sockfd,5);	//5 simultaneous connection at most
-     
-     //accept connections
+
+     listen(sockfd,10);	//5 simultaneous connection at most
+
+     /*//accept connections
      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-         
-     if (newsockfd < 0) 
-       error("ERROR on accept");
-         
-     int n;
-     char buffer[256];
-   			 
-     memset(buffer, 0, 256);	//reset memory
-      
-     //read client's message
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
 
-     //parse message here
-     http_r* request;
-     request = parseRequest(buffer);
-	 char* filePath = request->URI;
-     printf("Here is the file path: %s\n", filePath);
-     ////old code
-     // char responseMessage[255+MAX_FILE_SIZE];
-     // memset(responseMessage,0,255+MAX_FILE_SIZE);
-     // generateResponseMessage(filePath,255,responseMessage);
-     char * responseMessage = generateResponseMessage(request);
+     if (newsockfd < 0)
+       error("ERROR on accept");*/
 
-   	 //reply to client
-   	 n = write(newsockfd,responseMessage,strlen(responseMessage));
-   	 if (n < 0) error("ERROR writing to socket");
-         
-     
-     close(newsockfd);//close connection 
+
+     fd_set active_fd_set; //set for holding sockets
+     int newsockfd; //socket representing client
+
+     /* Initialize the set of active sockets */
+     FD_ZERO(&active_fd_set);
+     FD_SET(sockfd, &active_fd_set); //put sock to the set to monitor new connections
+
+     while(1){
+    	 if(select(FD_SETSIZE,&active_fd_set,NULL,NULL,NULL)<0){exit(-1);}//FAIL with exit status 1 on error
+
+
+   		 if(FD_ISSET(sockfd, &active_fd_set))//new connection request
+	     {
+	         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	         FD_SET(newsockfd, &active_fd_set);
+	     }
+	     if(FD_ISSET(newsockfd, &active_fd_set)){
+	    	 int n;
+	    	 char buffer[256];
+	 		 memset(buffer, 0, 256);	//reset memory
+
+	 		 //read client's message
+	 		 n = read(newsockfd,buffer,255);
+	 		 if (n < 0) error("ERROR reading from socket");
+	 		 printf("Here is the message: %s\n",buffer);
+
+	 		 //parse message here
+	 		 http_r* request;
+	 		 request = parseRequest(buffer);
+	 		 char* filePath = request->URI;
+	 		 printf("Here is the file path: %s\n", filePath);
+	 		 char * responseMessage = generateResponseMessage(request);
+
+	 		 //reply to client
+	 		 n = write(newsockfd,responseMessage,strlen(responseMessage));
+	 		 if (n < 0) error("ERROR writing to socket");
+	 		 close(newsockfd);//close connection
+	 	}
+     }
      close(sockfd);
-         
-     return 0; 
+     return 0;
 }
-
-/*
-void generateResponseMessage(char *filePath, int size, char *responseMessage)
-{
-    char fileMessage[MAX_FILE_SIZE];
-    int temp = MAX_FILE_SIZE;
-    int outputSize = 0;
-
-    memset(fileMessage,0,MAX_FILE_SIZE);
-    fileToMessage(filePath, fileMessage);
-    outputSize =  sprintf(responseMessage,
-"HTTP/1.1 200 OK\n\
-Connection: close\n\
-Date: \n\
-Server: CS118/0.0.1\n\
-Content-Length: %d\n\
-Content-Type: %s\n\
-\n\
-%s",
-(int)strlen(fileMessage), "text/html", fileMessage);
-    printf("**gRM, outputSize=%d,%s\n",outputSize,responseMessage);
-}
-
-//SPECIAL CASE: when the file path is empty, open index.html
-//WARNING: need to deal with any file size, but right now it's confined to 1024 bytes
-void fileToMessage(char *filePath, char *fileMessage)
-{
-  FILE *pFile;
-  pFile = fopen(&filePath[1], "r");
-  char temp[50],fileLine[MAX_FILE_SIZE];
-
-  memset(temp,0,50);
-  memset(fileLine,0,MAX_FILE_SIZE);
-  memset(fileMessage,0,MAX_FILE_SIZE);
-  if(pFile == NULL)
-  {
-    sprintf(temp,"ERROR, could not open file \"%s\"\n",filePath);
-    error(temp);
-  }
-  else
-  {
-    while (fgets (fileLine, MAX_FILE_SIZE, pFile) != NULL )
-    {
-      strncat(fileMessage,fileLine,MAX_FILE_SIZE-strlen(fileMessage));
-    }
-    fclose (pFile);
-  }
-}
-*/
